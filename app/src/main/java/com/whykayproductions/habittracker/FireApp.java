daeab04d4b100ca9b5c0698ef24fc660c003f79d
youngkwanglee <lyoungkwang.ly@gmail.com>
2018-06-14 20:55:31 -0400

    Created FireApp class

---
+++ app/src/main/java/com/whykayproductions/habittracker/FireApp.java
@@ -0,0 +1,12 @@
+package com.whykayproductions.habittracker;
+
+import android.app.Application;
+
+public class FireApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
